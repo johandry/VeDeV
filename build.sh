22a8af4dd60a77b983ae8eaa77f5c17a2c6b3d72
Johandry Amador <johandry@gmail.com>
2014-12-02 17:41:33 -0600

    Parameters build and clean in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 
 ################################################################################
@@ -25,13 +25,21 @@ declare CASK=
 # Distro to build
 declare DISTRO=
 
+# Script name and directory
+declare -r SCRIPT_NAME=${0##*/}
+declare -r SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+# Options
+declare DEBUG=
+declare ACTION=
+
 #===============================================================================
 # Functions
 #===============================================================================
 
 # Print error message in red
 error () {
-  echo -e "\033[91;1mERROR:\033[0m ${1}"
+  echo -e "\033[91;1mERROR:\033[0m ${1}" >&2
 }
 
 # Print warning message in yellow
@@ -41,12 +49,148 @@ warning () {
 
 # Display help information abut how to use this script
 usage () {
-  :
+  cat<<-EOH
+${SCRIPT_NAME} [-v] -h | -c { all | box | cache | vagrant} | -l | distro
+where:
+  -h: Help. Print this information
+  -v: Verbose. Show more details when the script is executed. Useful for debugging
+  -l: List distributions available to build.
+  -c: Clean. Move to the trash directory the boxes, packer cache, vagrant boxes or all.
+  -b: Build a Linux Distribution. Could be the full name or partial but if should not match with more than one.
+EOH
+}
+
+# Check if the distro exists or if there are more than one with that name
+check_distro () {
+  json="${SCRIPT_DIR}/packer/templates/${DISTRO}*.json"
+  json=$(echo $json | sed "s|${SCRIPT_DIR}/packer/templates/||g" | sed 's/\.json//g')
+  count=$(echo ${json} | wc -w | sed 's/ //g')
+  if [[ ${count} -gt 1 ]]
+    then
+    error "There are $count distros with the name '${DISTRO}'. Specify which one you want to build."
+    for j in ${json}
+    do
+      echo "  * $j"
+    done
+    exit 1
+  fi
+  DISTRO=${json}
+}
+
+# List all the available distros to build
+list_distros () {
+  json="${SCRIPT_DIR}/packer/templates/*.json"
+  json=$(echo $json | sed "s|${SCRIPT_DIR}/packer/templates/||g" | sed 's/\.json//g')
+  for distro in ${json}
+  do
+    echo "  * ${distro}"
+  done
+}
+
+clean_boxes () {
+  d=$(date +%Y%m%d)
+  warning "Moving $(\ls -1 *.box | wc -l | sed 's/ //g') boxes to Trash/${d}"
+  mkdir -p "${SCRIPT_DIR}/Trash/${d}"
+  mv -f "${SCRIPT_DIR}"/*.box "${SCRIPT_DIR}"/Trash/${d}/
+}
+
+clean_cache () {
+  d=$(date +%Y%m%d)
+  warning "Moving Packer Cache to Trash/${d}"
+  mkdir -p "${SCRIPT_DIR}/Trash/${d}"
+  mv "${SCRIPT_DIR}"/packer_cache "${SCRIPT_DIR}"/Trash/${d}/
+}
+
+# Move to trash every box and remove every vagrant environment created.
+clean_vagrant() {
+  d=$(date +%Y%m%d)
+  mkdir -p "${SCRIPT_DIR}/Trash/${d}"
+  total=$(${VAGRANT} box list | wc -l | sed 's/ //g')
+  count=$(\ls -1d "${SCRIPT_DIR}"/vagrant/* | wc -l | sed 's/ //g')
+  for box in "${SCRIPT_DIR}"/vagrant/*
+  do
+    PWD=$(pwd)
+    cd ${box}
+    warning "Destroying vagrant box 'vagrant-${box##*/}'"
+    ${VAGRANT} destroy -f
+    warning "Removing vagrant box 'vagrant-${box##*/}'"
+    ${VAGRANT} box remove vagrant-${box##*/}
+    warning "Moving vagrant box 'vagrant-${box##*/}' to Trash/${d}"
+    mv "${box}" "${SCRIPT_DIR}"/Trash/${d}/
+  done
+  echo
+  warning "Removed ${count}/${total} vagrant boxes"
+}
+
+clean_all () {
+  clean_cache
+  echo
+  clean_boxes
+  echo
+  clean_vagrant
+  echo
 }
 
 # Process the options or parameters for this script
 process_opt () {
-  :
+  [[ $# -eq 0 ]] && usage && exit 1
+
+  declare -r options=":hvlc:"
+
+  while getopts "${options}" opt; do
+    case "${opt}" in
+      h)
+        usage
+        exit 0
+        ;;
+      v)
+        DEBUG=1
+        ;;
+      l)
+        list_distros
+        exit 0
+        ;;
+      c)
+        case ${OPTARG} in
+          box)
+            ACTION=clean_boxes
+            ;;
+          cache)
+            ACTION=clean_cache
+            ;;
+          vagrant)
+            ACTION=clean_vagrant
+            ;;
+          all)
+            ACTION=clean_all
+            ;;
+          \?)
+            error "Invalid argument '-${OPTARG}'. Clean options could be: box, cache, vagrant and all"
+            usage
+            exit 1
+            ;;
+          esac
+          ;;
+      \?)
+        error "Invalid argument '-${OPTARG}'"
+        usage
+        exit 1
+        ;;
+      *)
+        usage
+        exit 0
+        ;;
+    esac
+  done
+  shift $((OPTIND-1))
+  
+  if [[ -z ${ACTION} ]]
+    then
+    [[ $# -eq 0 ]] && error "No distro provided" && usage && exit 1
+    DISTRO="${1}"
+    check_distro
+    ACTION=build
+  fi
 }
 
 # Check requirements for Mac OSX
@@ -166,22 +310,36 @@ check_requirements () {
 
 # Build the box for the requested distro using packer
 packer_build () {
-  :
+  echo "Building box for ${DISTRO}"
+  echo
+  ${PACKER} build "${SCRIPT_DIR}/packer/templates/${DISTRO}.json"
 }
 
 # Build the Vagrant box for the requested distro previously builded with Packer
 vagrant_build () {
-  :
+  echo "Creating vagrant environment"
+  echo
+  ${VAGRANT} box add "vagrant-${DISTRO}" "${SCRIPT_DIR}/${DISTRO}.box"
+  mkdir -p "${SCRIPT_DIR}/vagrant/${DISTRO}/${DISTRO}.box" && cd "${SCRIPT_DIR}/vagrant/${DISTRO}"
+  ${VAGRANT} init "vagrant-${DISTRO}"
+  ${VAGRANT} up
 }
 
 #===============================================================================
 # Main code
 #===============================================================================
 
-process_opt
+process_opt "$@"  # Process the parameters
 
-check_requirements
+check_requirements # Check if the required software exists and take their full path
 
-packer_build
+if [[ ${ACTION} == "build" ]] # then build the packer box and the vagrant box
+  then
+  packer_build
+  vagrant_build
+elif [[ -n ${ACTION} ]] # if there is an action, execute it. This will be a clean funciton
+  then
+  ${ACTION}
+fi
 
 exit 0
