55d4ea0c734321e55d826ae674091406bdd20697
Johandry Amador <johandry@gmail.com>
2015-08-21 21:50:20 -0700

    Last Sync

--- packer/scripts/ubuntu/base.sh
+++ packer/scripts/ubuntu/base.sh
@@ -8,5 +8,7 @@ message () {
 message "Setting one active console"
 sed -i -e 's,^\(ACTIVE_CONSOLES="/dev/tty\).*,\11",' /etc/default/console-setup
 for f in /etc/init/tty[^1]*.conf; do
-  mv "$f"{,.bak}
+  [[ -e "$f" ]] && mv "$f"{,.bak}
 done
+
+exit 0
